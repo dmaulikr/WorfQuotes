7d37d072f7a7d6010184cc25429cb571f0d07fcf
Paul Greidanus <paul@majestik.org>
2015-11-11 13:45:52 -0700

    Initial Commit

---
+++ Worf Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Worf Quotes
+//
+//  Created by Paul Greidanus on 2015-11-11.
+//  Copyright © 2015 Paul Greidanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
